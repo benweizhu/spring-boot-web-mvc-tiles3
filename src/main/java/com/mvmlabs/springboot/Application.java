85ac1733cfcbd25aa582c2cd49ad91dab70b6323
Benwei Zhu <benweizhu@gmail.com>
2016-02-09 15:11:32 +0800

    make war can be deploy to tomcat

--- src/main/java/com/mvmlabs/springboot/Application.java
+++ src/main/java/com/mvmlabs/springboot/Application.java
@@ -2,18 +2,25 @@ package com.mvmlabs.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Spring Source Tool Suite.
- * 
+ *
  * @author Mark Meany
  */
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
